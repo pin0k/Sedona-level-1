c0205984c2e06b0b50369868c6893f21d81df74a
Абакумова Анна <pin0k@yandex.ru>
2022-01-03 20:43:46 +0300

    Добавление localStorage для хранения количества взрослых и детей

--- js/search.js
+++ js/search.js
@@ -1,17 +1,44 @@
 const searchLink = document.querySelector(".button-search");
 const searchPopup = document.querySelector(".modal-search");
 const appointmentForm = searchPopup.querySelector(".appointment-form");
-const inputArrivalDate = searchPopup.querySelector("input[name=arrival_date]");
-const inputDepartureDate = searchPopup.querySelector("input[name=date_of_departure]");
+const inputArrivalDate = searchPopup.querySelector("input[name=arrival-date]");
+const inputDepartureDate = searchPopup.querySelector("input[name=date-of-departure]");
+const inputAdults = searchPopup.querySelector("input[name=adults]");
+const inputChildren = searchPopup.querySelector("input[name=children]");
+
+let isStorageSupport = true;
+let storageAdults = "";
+let storageChildren = "";
+
+try {
+  storageAdults = localStorage.getItem("adults");
+  storageChildren = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (storageAdults) {
+  inputAdults.value = storageAdults;
+}
+
+if (storageChildren) {
+  inputChildren.value = storageChildren;
+}
 
 searchLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   searchPopup.classList.toggle("modal-show");
+
   inputArrivalDate.focus();
 });
 
 appointmentForm.addEventListener("submit", function (evt) {
   if (!inputArrivalDate.value || !inputDepartureDate.value) {
     evt.preventDefault();
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adults", inputAdults.value);
+      localStorage.setItem("children", inputChildren.value);
+    }
   }
 });
